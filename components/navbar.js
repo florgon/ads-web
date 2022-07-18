cc3547d711c883a0a13394d7c10e9a4f3f5f365a
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-18 15:40:42 +0400

    Add navbar profile username.

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,9 @@
 import {Navbar as BootstrapNavbar, Nav as BootstrapNav, Container, Button} from 'react-bootstrap';
 import Link from 'next/link'
+import useAuth from '../contexts/auth';
 
 export default function Navbar(){
+    const { user, loading, isAuthenticated } = useAuth();
     return (
         <BootstrapNavbar bg="light" expand="sm" className="border-bottom container-fluid navbar shadow-sm">
             <Container fluid>
@@ -14,7 +16,12 @@ export default function Navbar(){
             </BootstrapNavbar.Brand>
             <BootstrapNavbar.Collapse id="navbarCollapse">
                 <BootstrapNav className="justify-content-end" style={{ width: "100%" }}>
-                    <Link href="/profile"><Button variant="outline-primary">Dashboard</Button></Link>&nbsp;
+                    {isAuthenticated && 
+                        <Link href="/profile"><Button variant="outline-primary">Signed as {user.username}</Button></Link>
+                    }
+                    {!isAuthenticated &&
+                        <Link href="/profile"><Button variant="outline-primary">Sign in</Button></Link>
+                    }&nbsp;
                     <Link href="https://florgon.space"><Button variant="outline-primary">Go to Florgon</Button></Link>
                 </BootstrapNav>
             </BootstrapNavbar.Collapse>
